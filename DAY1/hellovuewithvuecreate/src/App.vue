a6b8ada5bf5b1a6845d38a68cf18b8d44ca322c4
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-02-26 15:07:48 +0530

    ✨ Adding scoped Styles & using bind with images

--- DAY1/hellovuewithvuecreate/src/App.vue
+++ DAY1/hellovuewithvuecreate/src/App.vue
@@ -2,53 +2,22 @@
 import Product from "./components/Product.vue"
 
 const productList = [
-  { name: "MacBook Pro", price: 300000 },
-  { name: "MacBook Air", price: 200000 },
-
+  { name: "MacBook Pro", price: 300000, imageUrl: "https://www.zdnet.com/a/img/2023/11/06/ff2ab50d-93b8-4954-96e5-7176557f03b5/dsc02399-enhanced-nr.jpg" },
+  { name: "MacBook Air", price: 200000, imageUrl: "https://static.toiimg.com/thumb/resizemode-4,width-1280,height-720,msid-100982979/100982979.jpg" },
+  { name: "iPhone", price: 100000, imageUrl: "https://images.macrumors.com/t/ui4nZwmG_LjE6Sx-trb4zTIlK9M=/1600x900/smart/article-new/2023/09/iPhone-15-General-Feature-Black.jpg" },
+  { name: "Apple Watch", price: 30000, imageUrl: "https://www.apple.com/newsroom/images/2023/09/apple-introduces-the-advanced-new-apple-watch-series-9/article/Apple-Watch-S9-hero-230912_Full-Bleed-Image.jpg.large.jpg" },
+  { name: "iPod", price: 3000, imageUrl: "https://www.cnet.com/a/img/resize/c4dcbc39b278e6cd33e947dc50181b2c7ae2af91/hub/2016/05/10/2c982d4c-9aab-4e9b-a0ba-514c12c338ba/apple-ipod-shuffle-5614-007.jpg?auto=webp&fit=crop&height=675&width=1200" },
 ]
 
 </script>
 
 <template>
-  <header>
-    <div>
-      <div v-for="product in productList">
-        <Product :details="product" />
-      </div>
+  <div v-for="product in productList">
+    <Product :details="product" />
+  </div>
 
 
-      <!-- <Product /> -->
-
-
-    </div>
-  </header>
+  <!-- <Product /> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
--- DAY1/hellovuewithvuecreate/src/components/Product.vue
+++ DAY1/hellovuewithvuecreate/src/components/Product.vue
@@ -1,6 +1,9 @@
 <template>
-    <h2>{{ details.name }}</h2>
-    <h4>{{ details.price }}</h4>
+    <div class="product-card">
+        <img height="200px" width="350px" :src="details.imageUrl" alt="">
+        <h2>{{ details.name }}</h2>
+        <h4>{{ details.price }}</h4>
+    </div>
 </template>
 <script setup>
 defineProps({
@@ -13,4 +16,11 @@ defineProps({
 
 
 </script>
-<style></style>
+<style scoped>
+.product-card {
+    border: 2px solid gray;
+    border-radius: 5px;
+    margin: 5px;
+    padding: 5px;
+}
+</style>
