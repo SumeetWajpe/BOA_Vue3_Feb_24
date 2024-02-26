2caaa1fffa60b6e6a6f6562305ae81bcb5f2f7c8
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-02-26 16:08:55 +0530

    📝 adding fontawesome cdn & using icon

--- DAY1/hellovuewithvuecreate/src/components/Product.vue
+++ DAY1/hellovuewithvuecreate/src/components/Product.vue
@@ -1,8 +1,15 @@
 <template>
-    <div class="col-sm-6 col-md-4 g-2">
-        <img height="200px" width="300px" :src="details.imageUrl" alt="">
-        <h2>{{ details.name }}</h2>
-        <h4>{{ details.price }}</h4>
+    <div class="col-sm-6 col-md-3 g-2">
+        <div class="card">
+            <img height="200px" width="300px" class="card-img-top" :src="details.imageUrl" :alt="details.name">
+            <div class="card-body">
+                <h5 class="card-title">{{ details.name }}</h5>
+                <p class="card-text">₹.{{ details.price }}</p>
+                <p class="card-text">{{ details.rating }}</p>
+                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i>
+
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
