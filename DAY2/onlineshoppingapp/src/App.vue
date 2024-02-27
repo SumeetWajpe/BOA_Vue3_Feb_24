d6f7abe28acb2711424eeb368fdaa81139e44c61
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-02-27 12:51:30 +0530

    ✨ using router link

--- DAY2/onlineshoppingapp/src/App.vue
+++ DAY2/onlineshoppingapp/src/App.vue
@@ -1,10 +1,17 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
-import ListOfProducts from './components/ListOfProducts.vue';
 
 </script>
 
 <template>
+  <div>
+    <!-- <a href="/">Home</a> |
+    <a href="/hello">Hello World</a> -->
+
+    <router-link to="/">Home</router-link> |
+    <router-link to="/hello">Hello World</router-link>
+
+
+  </div>
   <!-- One one default router, multiple named routerview -->
   <router-view></router-view>
 </template>
