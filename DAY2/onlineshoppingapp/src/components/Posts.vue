59a250c2c05fd0d8f0e109493e33f92541632731
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-02-27 16:06:49 +0530

    ✨ display post in UI

--- DAY2/onlineshoppingapp/src/components/Posts.vue
+++ DAY2/onlineshoppingapp/src/components/Posts.vue
@@ -2,14 +2,22 @@
     <header>
         <h1>All Posts</h1>
     </header>
+    <ul class="list-group">
+        <li class="list-group-item" v-for="post in postsData.posts" :key="post.id">
+            {{ post.title }}
+        </li>
+    </ul>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { onMounted, reactive } from 'vue'
 import axios from 'axios'
+const postsData = reactive({
+    posts: []
+});
 onMounted(async () => {
     const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts")
-    console.log(data)
+    postsData.posts = data
 })
 </script>
 
