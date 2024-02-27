49cf981903c8732350c6095dcf4c1e20e20255a9
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-02-27 16:58:31 +0530

    ✨ displaying post Details in postDetails Component

--- DAY2/onlineshoppingapp/src/components/PostDetails.vue
+++ DAY2/onlineshoppingapp/src/components/PostDetails.vue
@@ -1,14 +1,29 @@
 <template>
     <header>
-        <h1>Post Details</h1>
+        <h1>Post Details for {{ postData?.post?.id }}</h1>
     </header>
+    <main class="border border-2 p-2">
+        <p>User Id : {{ postData?.post?.userId }} </p>
+        <p>Title : {{ postData?.post?.title }} </p>
+        <p>Body : {{ postData?.post?.body }} </p>
+
+    </main>
 </template>
 
 <script setup>
 import { useRoute } from "vue-router"
+import { reactive, onMounted } from "vue"
+import axios from "axios"
 
 const route = useRoute()
-console.log(route.params.id)
+const postData = reactive({
+    post: {}
+});
+
+onMounted(async () => {
+    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${route.params.id}`)
+    postData.post = data
+})
 </script>
 
 <style scoped></style>
